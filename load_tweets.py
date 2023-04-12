ba01b454ed54736a033ac59c31d59da573f271c4
Abdoul Hanane Gbadamassi <49354774+afroCoderHanane@users.noreply.github.com>
2023-04-11 23:39:12 -0700

    Update load_tweets.py

--- load_tweets.py
+++ load_tweets.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 # process command line args
-import argpars
+import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--db',required=True)
 parser.add_argument('--inputs',nargs='+',required=True)
